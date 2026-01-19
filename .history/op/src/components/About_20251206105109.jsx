ff93d596385bd2478f22fa75375ac64c747ac3bb
Ayush Sharma <your-email@example.com>
2026-01-19 17:01:10 +0530

    Fix: remove .history and submodule issue

--- .history/op/src/components/About_20251206105109.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-  return (
-    <div className='h-[100vh] w-f'>About</div>
-  )
-}
-
-export default About
--- .history/op/vite.config_20251204124757.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-npm install tailwindcss @tailwindcss/vite
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
